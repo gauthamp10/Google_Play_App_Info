648a9ce4fc5a6c3382da850cef9beda22099c3f4
Gautham Prakash <31800012+gauthamp10@users.noreply.github.com>
2019-04-07 10:57:44 +0530

    Added new features
    App SIze,Number of installs,Minimum Android Version,Last Updated,Latest version etc...

--- src/fetch.py
+++ src/fetch.py
@@ -1,39 +1,39 @@
 import os
 import re
-import requests  
+import requests
 from bs4 import BeautifulSoup
 ch=''
 def main():
-	
+
 	def screen_clear():                                   #clearing terminal screen
 		if os.name == 'nt':
 			os.system('cls')
 		else:
 			os.system('clear')
-			
+
 	def intro():
 		print("\t\t\t\t\t-----Google Play App Info------")
 		box_msg(''' Created by Gautham Prakash @: gauthamp10@gmail.com''')
-		
-		
+
+
 	def box_msg(msg):                                     #for printing text in a box
 		row = len(msg)
 		h = ''.join(['+'] + ['-' *row] + ['+'])
 		result= h + '\n'"|"+msg+"|"'\n' + h
-		print(result)	
-		
+		print(result)
+
 	def print_lines():                                    #printing dotted lines
-		print(("-"*68))	
-	
+		print(("-"*68))
+
 	def get_app_link(query):
 		html_page = requests.get("https://play.google.com/store/search?q="+query+"&c=apps")
 		soup = BeautifulSoup(html_page.text,'html.parser')
 		applink=soup.find('a',attrs={'class':'card-click-target'})
 		return applink['href']
-		
+
 	def get_info(url,soup):
-		name=soup.find('meta',attrs={'itemprop':'name'}) 
-		icon=soup.find('img',attrs={'itemprop':'image'})
+		info_box = [info_box.text.strip() for info_box in soup.find_all('div', 'hAyfc')]
+		name=soup.find('meta',attrs={'itemprop':'name'})
 		price=soup.find('meta',attrs={'itemprop':'price'})
 		priceCurrency=soup.find('meta',attrs={'itemprop':'priceCurrency'})
 		desc=soup.find('meta',attrs={'itemprop':'description'})
@@ -42,25 +42,38 @@ def main():
 		ratingValue=soup.find('meta',attrs={'itemprop':'ratingValue'})
 		reviewCount=soup.find('meta',attrs={'itemprop':'reviewCount'})
 		availability=soup.find('meta',attrs={'itemprop':'availability'})
-		updated=soup.find('span',attrs={'class':'htlgb'})
+		for item in info_box:
+			if item.startswith("Updated"):
+				updated=str(item[7:])
+			if item.startswith("Size"):
+				appSize=str(item[4:])
+			if item.startswith("Installs"):
+				installCount=str(item[8:])
+			if item.startswith("Requires Android"):
+				minAndroid=str(item[16:])
+			if item.startswith("Current Version"):
+				curVersion=str(item[15:])
 		print("App name: ",name['content'])
-		print("Icon url: ",icon['src'])
 		print("Price: ",price['content']+priceCurrency['content'])
 		print("Description: ",desc['content'][:80]+".....")
 		print("Content Rated for: ",contentRating['content'])
 		print("Category: ",applicationCategory['content'])
 		print("Rating: ",ratingValue['content'])
 		print("Total Reviews: ",reviewCount['content'])
-		print("Last Updated on: ",updated.text)
 		print("Availability: ",availability['content'].rsplit('/',1)[1])
 		print("App URL: ",url)
-	
+		print("Last Updated: ",updated)
+		print("Size: ",appSize)
+		print("Installs: ",installCount)
+		print("Minimum Android Required: ",minAndroid)
+		print("Lastest Version: ",curVersion)
+
 	screen_clear()
-	intro()	
+	intro()
 	query=input("Enter the app name: ")
 	query=query.replace(' ','+')
 
-	try:	
+	try:
 		url='https://play.google.com'+get_app_link(query)
 		print_lines()
 		data=requests.get(url)
@@ -68,9 +81,9 @@ def main():
 		get_info(url,soup)
 	except:
 		print("Network Error| Unknown App!...")
-	print_lines()	
-	
+	print_lines()
+
 if __name__ == '__main__':                                       #Calling main(), the actual entry point for the scraper
-    
+
 	main()
 	exit(0)
